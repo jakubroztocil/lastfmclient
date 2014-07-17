8cff3c4f41bc6110a8c5f238b1c1f2f9a16256dc
Tsukasa Hiiragi <anonymous-kun@li.ru>
2014-07-17 15:21:11 +0300

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -16,5 +16,15 @@ setup(
     packages=['lastfmclient'],
     install_requires=[
         'requests>=1.0.4'
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
